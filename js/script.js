fd906e20bb7bc7080e3d9f56be91e862e7e9e933
Gagan Singh <spokanetaxicabs@gmail.com>
2017-06-02 22:05:28 -0700

    Project 2 done
    Project 2 done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let pageSize = 10; // how many students should be on the page.
 let pageCount = Math.ceil(studentList.length/pageSize); // geting the page numbers rounded.
 const $paginationDiv = $('.pagination'); //selecting the class pagination
 
+
 // creating the pages for the how many students are there.
 function pagination()  {
   let ul = $('<ul></ul>'); //creating unorder list tags
@@ -16,38 +17,36 @@ function pagination()  {
 }
 pagination();
 // showing the 10 students.
-let listStudents = (pageNum, studentList) => {
+let listStudents = (pageNum) => {
   $(studentList).hide() //hiding the students
-   let students = []; //creating empty array for the students
+  let student =[]; //creating empty array for the students
     for (let i = 0; i < studentList.length; i++){ // looping over the student list
-      if ( i  < pageNum * pageSize && i > (pageNum - 1) * pageSize - 1){
-      students.push(studentList[i]); // pushing the 10 students to page
+      if ( i >= pageNum * pageSize && i <= (pageNum * pageSize) + pageSize - 1){
+       student.push($(studentList[i]))// pushing the 10 students to page
      $(studentList[i]).show(); // showing the students  that are suppose to be on the page.
 
     }
   }
-   return students;
+  return student;
 }
-$(listStudents(1, studentList));
+$(listStudents(0));
 
 // Defines which students show up when each page is clicked.
 function displayPage() {
   // let student = [];
   let $pages = $('.pagination ul li'); //selecting class pagination unoreder list and list inside.
-  for (let i = 1; i < pageCount; i++) { // looping over the page until i is less than the pageCount.
+  for (let i = 0; i < pageCount; i++) { // looping over the page until i is less than the pageCount.
     $($pages[i]).click(function() { //when clicked on each page..
-         studentList[i]; //
+        return $(listStudents(i));
     });
-    // return student;
+
   }
 }
 
 displayPage();
 
-  let searchStudent = () => {
-     let searchInput = $('<form></form>');
-     let input = $("<input>")
-     $(searchInput).append(input);
+// let searchStudent = () => {
+//     let searchInput = $("<input></input>")
 //     let studentEmail = $("#email");
 //     let studentName = $("h3");
 //     if (studentEmail === student-item){
@@ -57,4 +56,4 @@ displayPage();
 //           alert("This user is not in the list of the students");
 //         }
 //       }
-     }
+//     }
